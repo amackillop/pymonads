57a9c909bccaa9b18fed8d2f51e8636e10839407
amackillop <austin.mackillop@gmail.com>
2019-09-14 19:12:52 -0400

    updated env, added a curry decorator

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-from typing import TypeVar
+import functools
+from typing import TypeVar, Callable, Any
 
 A = TypeVar('A')
+Func = TypeVar('Func', bound=Callable[..., Any])
 
 def identity(value: A) -> A:
     """The identity function"""
@@ -9,3 +11,11 @@ def identity(value: A) -> A:
 def const(value: A, _) -> A:
     """ Simply returns it's first argument"""
     return value
+
+def curry(func: Func):
+    @functools.wraps(func)
+    def curried(*args, **kwargs):
+        if func.__code__.co_argcount <= len(args):
+            return func(*args, **kwargs)
+        return functools.partial(func, *args, **kwargs)
+    return curried
