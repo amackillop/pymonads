edc1f9631cf95fdfa14460a88fa8e90c36525c82
amackillop <austin.mackillop@gmail.com>
2019-09-15 21:45:35 -0400

    Polymorphism instead of pattern matching

--- applicative.py
+++ applicative.py
@@ -18,11 +18,6 @@ D = TypeVar('D')
 class Applicative(Functor, Generic[T_co], metaclass=abc.ABCMeta):
     """An applicative functor"""
 
-    @property
-    @abc.abstractmethod
-    def value(self):
-        """"""
-
     @classmethod
     @abc.abstractmethod
     def pure(cls, value: A) -> Applicative[A]:
--- either.py
+++ either.py
@@ -12,26 +12,12 @@ E = TypeVar('E')
 from monad import Monad
 from applicative import Applicative
 
-class Either(Monad[Union[E,T]], Generic[E, T], metaclass=abc.ABCMeta):
+class Either(Monad, Generic[E, T], metaclass=abc.ABCMeta):
     """Etiher Monad"""
 
-    @property
-    @abc.abstractmethod
-    def value(self):
-        """The wrapped value"""
-
-    @classmethod
-    def pure(cls, value: A):
+    @staticmethod
+    def pure(value: A) -> Right[A]:
         return Right(value)
-
-    def fmap(self, func: Callable[..., B]) -> Either[A, B]:
-        return Right(func(self.value)) if isinstance(self, Right) else self
-
-    def amap(self, fab: Applicative[Callable[..., B]]) -> Either[A, B]:
-        return self.fmap(fab.value)
-
-    def flat_map(self, func: Callable[..., Either[A, B]]) -> Either[A, B]:
-        return func(self.value) if isinstance(self, Right) else self
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.value})'
@@ -41,10 +27,33 @@ class Left(Either, Generic[A]):
     """"""
     value: A
 
+    def fmap(self, func: Callable[..., B]) -> Left[A]:
+        return self
+
+    def amap(self, fab: Applicative[Callable[..., B]]) -> Left[A]:
+        return self
+
+    def flat_map(self, func: Callable[..., Monad[B]]) -> Left[A]:
+        return self
+
+
 @dataclass(frozen=True, repr=False)
 class Right(Either, Generic[B]):
     """"""
     value: B
+
+    def fmap(self, func: Callable[..., A]) -> Right[A]:
+        return Right(func(self.value))
+
+    def amap(self, fab: Applicative[Callable[..., A]]) -> Right[A]:
+        return self.fmap(fab.value)
+
+    def flat_map(self, func: Callable[..., Either[A, B]]) -> Either[A, B]:
+        return func(self.value)
+
+a = Right(1)
+
+a.flat_map(lambda x: Left(3))
 
 # @dataclass(frozen=True, repr=False)
 # class Right(Either, Generic[T]):
