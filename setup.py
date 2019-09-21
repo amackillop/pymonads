71c61d408ff2f84a807ecc20f509583a9b85c813
amackillop <austin.mackillop@gmail.com>
2019-09-21 09:51:02 -0400

    Restructuring

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='pymonads',
-        version='0.1',
-        description='Well typed functional programming abstactions in Python',
-        url='http://github.com/amackillop/pymonads',
-        author='Austin Mackillop',
-        author_email='austin.mackillop@gmail.com',
-        license='MIT',
-        package_dir={'': 'src'},
-        packages=find_packages('src'),
-        zip_safe=True)
+      version='0.1.1',
+      description='Well typed functional programming abstactions in Python',
+      url='http://github.com/amackillop/pymonads',
+      author='Austin Mackillop',
+      author_email='austin.mackillop@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=('tests', 'docs')),
+      zip_safe=True)
