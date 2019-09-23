cf21e420305c9897684b73dd9b8e9edaa3d7fd38
amackillop <austin.mackillop@gmail.com>
2019-09-22 22:28:30 -0400

    factored out common code in either

--- pymonads/either.py
+++ pymonads/either.py
@@ -20,11 +20,11 @@ C = TypeVar('C')
 E = TypeVar('E')
 T = TypeVar('T')
 
-
 @dataclass(frozen=True, repr=False)
-class Left(Monad[E]):
-    """Some representation of an error. Further computations will no longer happen."""
-    _value: E
+class _Either(Generic[T]):
+    """Common code between Left and Right"""
+
+    _value: T  
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.value})'
@@ -32,6 +32,10 @@ class Left(Monad[E]):
     @property
     def value(self):
         return self._value
+
+
+class Left(_Either, Monad[E]):
+    """Some representation of an error. Further computations will no longer happen."""
 
     def pure(self, value: A) -> Left[A]:
         return Left(value)
@@ -46,17 +50,8 @@ class Left(Monad[E]):
         return self
 
 
-@dataclass(frozen=True, repr=False)
-class Right(Monad[T]):
+class Right(_Either, Monad[T]):
     """A successful result that will propagate."""
-    _value: T
-    
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.value})'
-
-    @property
-    def value(self):
-        return self._value
 
     def pure(self, value: A) -> Right[A]:
         return Right(value)
@@ -70,24 +65,30 @@ class Right(Monad[T]):
     def flat_map(self, func: Callable[[T], Either[E, B]]) -> Either[E, B]:
         return func(self.value)
 
+
 Either = Union[Left[E], Right[T]]
 
 def pure(value: T) -> Right[T]:
     return Right(value)
 
+
 def is_left(either_: Either[E, A]) -> bool:
     return isinstance(either_, Left)
 
+
 def is_right(either_: Either[E, A]) -> bool:
     return isinstance(either_, Right)
+
 
 @curry
 def either(func_left: Callable[[E], C], func_right: Callable[[T], C], either_: Either) -> C:
     val = either_.value
     return func_right(val) if is_right(either) else func_left(val)
 
+
 def lefts(eithers: Iterable[Either]) -> Iterator[A]:
     return (either.value for either in eithers if is_left(either))
+
 
 def rights(eithers: Iterable[Either]) -> Iterator[B]:
     return (either.value for either in eithers if is_right(either))
