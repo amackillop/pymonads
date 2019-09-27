2fb75c82d2fbdbff4c4a331ecc1cf711722fd253
amackillop <austin.mackillop@gmail.com>
2019-09-27 00:42:52 -0400

    Applicative laws

--- pymonads/tests/test_either.py
+++ pymonads/tests/test_either.py
@@ -3,15 +3,20 @@ import hypothesis.strategies as st
 from typing import TypeVar
 
 import pymonads.tests.functor_laws as fl
+import pymonads.tests.applicative_laws as al
 from pymonads import Either, Right, Left
-from pymonads.either import pure
+from pymonads.either import pure, _Either
 from pymonads.utils import identity, compose
 
 A = TypeVar('A')
 B = TypeVar('B')
 
-def eithers():
+def eithers() -> st.SearchStrategy:
     return st.one_of(st.builds(Right, st.integers()), st.builds(Left, st.integers()))
+
+def eithers_applicative() -> st.SearchStrategy:
+    f = st.just(lambda x: x*4)
+    return st.builds(Right, f)
 
 # Test that Either obeys the functor laws.
 @given(either=eithers())
@@ -26,5 +31,16 @@ def test_either_preserves_composition_of_morphisms(either):
 
 # Test that Either obeys the applicative laws.
 @given(either=eithers())
-def test_applicative_identity_law(either):
-    assert either.amap(pure(identity)) == either
+def test_either_obeys_identity_law(either):
+    assert al.applicative_preserves_identity_morphism(either)
+
+@given(x=st.integers())
+def test_either_is_homomorphic(x):
+    f = lambda a: a * 4
+    assert al.applicative_preserves_function_application(_Either, f, x)
+
+@given(either=eithers_applicative(), x=st.integers())
+def test_either_obeys_interchangeability(either, x):
+    assert al.applicative_is_interchangeable(_Either, either, x)
+
+# Test that Either obeys the Monad laws.
